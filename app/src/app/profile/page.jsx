48324efa3c0e1a9b1520f3b9b0b78deeeb57197c
Devansh Arora <hsnaved.reverse@gmail.com>
2025-05-18 21:55:57 +0530

    enhanced profile and search ui

--- app/src/app/profile/page.jsx
+++ app/src/app/profile/page.jsx
@@ -3,10 +3,6 @@ import Profile from "../../components/profile/Profile";
 export default function ProfilePage() {
   return (
     <main>
-      <div className="page-header">
-        <h1>Profile</h1>
-        <p>Configure your profile</p>
-      </div>
       <Profile />
     </main>
   );
---
+++ app/src/components/DynamicWrapper/DynamicWrapper.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+export default function DynamicWrapper({ children }) {
+  const pathname = usePathname();
+
+	let bool = true;
+
+  if (pathname === "/" || pathname === "/learn/") {
+		bool = false;
+	}
+
+
+  return (
+    <div className={bool ? "lg:ml-[7rem]" : ""}>
+      {children}
+    </div>
+  );
+}
