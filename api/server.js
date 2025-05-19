8ec3535a77d5735e5fedefc7029e787201b3577f
Upayan M <upayanm3@gmail.com>
2025-05-19 11:53:23 +0530

    Log CORS options upon successful MongoDB connection for better debugging

--- api/server.js
+++ api/server.js
@@ -45,6 +45,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
+    console.log("CORS options:", corsOptions);
     app.listen(PORT, () =>
       console.log(
         `Server running on port ${PORT}\nAPI: http://localhost:${PORT}`
