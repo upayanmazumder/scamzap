ac54d1880d05d0619c4e7ca1477bf55fb7d5cb53
Upayan M <upayanm3@gmail.com>
2025-05-19 11:52:11 +0530

    Configure CORS options in server.js for production environment

--- api/server.js
+++ api/server.js
@@ -16,8 +16,12 @@ const app = express();
 const PORT = 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const corsOptions =
+  process.env.NEXT_PUBLIC_ENV === "production"
+    ? { origin: "https://scamzap.upayan.dev" }
+    : {};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
